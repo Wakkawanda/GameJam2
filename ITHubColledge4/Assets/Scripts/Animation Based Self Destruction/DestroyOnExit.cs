30e4667dd9aa8b430edd6ab1109582740ffecd67
Anatoly Ponomarev <i@foundfun.ru>
2024-06-16 00:45:56 +0500

    Add titul and fix death

--- ITHubColledge4/Assets/Scripts/Animation Based Self Destruction/DestroyOnExit.cs
+++ ITHubColledge4/Assets/Scripts/Animation Based Self Destruction/DestroyOnExit.cs
@@ -1,6 +1,10 @@
-using UnityEditor;
-public class DestroyOnExit : StateMachineBehaviour {
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        Destroy(animator.gameObject, stateInfo.length);
+using UnityEngine;
+
+namespace Animation_Based_Self_Destruction
+{
+    public class DestroyOnExit : StateMachineBehaviour {
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+            Destroy(animator.gameObject, stateInfo.length);
+        }
     }
 }
