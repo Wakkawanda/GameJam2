e8f9efcbaf4875f8241808a7c84e8a5816bc8c13
SemYs <steamacaunmy@mail.ru>
2024-06-16 16:59:31 +0500

    Merge branch 'stable' of https://github.com/Wakkawanda/GameJam2 into stable
    # Conflicts:
    #	ITHubColledge4/Assets/Music/smoke1.mp3.meta
    #	ITHubColledge4/Assets/Music/smoke2.mp3.meta

--- ITHubColledge4/Assets/Scripts/Lobby/buttonLobby.cs
+++ ITHubColledge4/Assets/Scripts/Lobby/buttonLobby.cs
@@ -8,12 +8,14 @@ using Button = UnityEngine.UI.Button;
 
 public class buttonLobby : MonoBehaviour
 {
-    [SerializeField] private Button play, autors, exit;
+    [SerializeField] private Button play, autors, exit, autorsexit;
+    [SerializeField] private GameObject creaters;
     public void OnEnable(){
 
         play.onClick.AddListener(() => SceneManager.LoadScene("Game"));
         exit.onClick.AddListener(Application.Quit);
         autors.onClick.AddListener(AutorsClick);
+        autorsexit.onClick.AddListener(AutorsExit);
 
 
     }
@@ -22,5 +24,13 @@ public class buttonLobby : MonoBehaviour
         play.gameObject.SetActive(false);
         autors.gameObject.SetActive(false);
         exit.gameObject.SetActive(false);
+        creaters.gameObject.SetActive(true);
+    }
+
+    private void AutorsExit(){
+        play.gameObject.SetActive(true);
+        autors.gameObject.SetActive(true);
+        exit.gameObject.SetActive(true);
+        creaters.gameObject.SetActive(false);
     }
 }
